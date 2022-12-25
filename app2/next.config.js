ae7dc54a62ee5b0a04afe3d1d9ca5b5292a0e70b
nerikosans <36780394+nerikosans@users.noreply.github.com>
2022-12-26 00:36:53 +0900

    Modify app2 config

--- app2/next.config.js
+++ app2/next.config.js
@@ -1,6 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  distDir: 'build',
+  webpack: (config, options) => {
+    config.plugins.push(
+      new options.webpack.container.ModuleFederationPlugin({
+        name: 'app2',
+        filename: 'remoteEntry_app2.js',
+        remoteType: 'var',
+        exposes: {
+          './app2Message': './components/App2Message',
+        },
+        shared: [
+          {
+            react: {
+              eager: true,
+              singleton: true,
+              requiredVersion: false,
+            },
+          },
+          {
+            'react-dom': {
+              eager: true,
+              singleton: true,
+              requiredVersion: false,
+            },
+          },
+        ],
+      })
+    );
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
