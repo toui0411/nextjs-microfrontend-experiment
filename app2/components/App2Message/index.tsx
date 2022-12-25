7517a9d5a5b590e11a0da1628e617594aa31faff
nerikosans <36780394+nerikosans@users.noreply.github.com>
2022-12-26 00:25:36 +0900

    Add App2Message

---
+++ app2/components/App2Message/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css';
+
+export const App2Message = () => {
+  return (
+    <section className={styles.container}>
+      <p>Hello from App 2!</p>
+    </section>
+  );
+};
